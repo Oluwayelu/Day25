da80d677a206f32dc7a58d0ca1123c0372a44122
Ifeoluwa Oluwayelu <46842207+Oluwayelu@users.noreply.github.com>
2020-04-23 18:21:15 +0100

    Update mongoUri

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     jwtSecret: "ecx-Secret",
-    mongoURI: "mongodb://localhost/ecxBackend"
+    mongoURI: "mongodb+srv://Ifeoluwa:Ifeoluwa@cluster0-m7exc.gcp.mongodb.net/test?retryWrites=true&w=majority"
 }
